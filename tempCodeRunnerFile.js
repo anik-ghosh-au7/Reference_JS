1ec1a3f6c6e117e4a9805c378371ad7374429c8b
Anik Ghosh <tech.anikghosh@gmail.com>
2020-04-27 23:26:49 +0530

    changes

--- tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-var list = {"start_word": "monk", "end_word": "monument", "word": "monkey"};
-// keysSorted = Object.keys(list).sort(function(a,b){return list[a]-list[b]})
-// console.log(keysSorted);
-
-
-// console.log("monk" < "monkey");
