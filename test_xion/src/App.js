707666c559cf23cb57bbc05ee64caaf97c51cbda
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2023-02-09 19:44:39 +0530

    .

---
+++ test_xion/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+	return (
+		<div className="App">
+			Test App
+			<div productid="50" className="xion-subscription-button-wrapper"></div>
+		</div>
+	);
+}
+
+export default App;
